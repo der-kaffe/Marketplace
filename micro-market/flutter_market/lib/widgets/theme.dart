ff5659804f59b6de0d5456ef0648104dde355bd1
Paolo Castañon <pcastanon2022@alu.uct.cl>
2025-09-03 23:54:12 +0800

    Primera etapa Chats + Mensajes, ademas se agrego la paleta de colores

---
+++ micro-market/flutter_market/lib/widgets/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF0075B4);
+  static const Color accentYellow = Color(0xFFEDC500);
+  static const Color grey = Color(0xc8c8c8c8);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
