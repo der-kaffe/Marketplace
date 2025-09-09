8c38a2982c0b8c52ab316adf62c1924c7f865223
juanCardenasU <juancardenas6217@gmail.com>
2025-09-08 23:01:55 -0300

    startup screen

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -8,24 +8,34 @@ import '../../screens/home_screen.dart';
 import '../../screens/favorites_screen.dart';
 import '../../screens/profile_screen.dart';
 import '../../screens/new_post_screen.dart';
+import '../../screens/startup.dart';
 
 // Admin
 import '../../screens/admin_menu_page.dart';
-import '../../screens/admin_users_page.dart'; 
+import '../../screens/admin_users_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/startup',
     routes: [
+      GoRoute(
+          path: '/startup', builder: (context, state) => const StartupScreen()),
       GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
 
       ShellRoute(
         builder: (context, state, child) => MainScreen(child: child),
         routes: [
-          GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
-          GoRoute(path: '/home/messages', builder: (context, state) => ConversationsPage()),
-          GoRoute(path: '/home/favorites', builder: (context, state) => const FavoritesScreen()),
-          GoRoute(path: '/home/profile', builder: (context, state) => const ProfileScreen()),
+          GoRoute(
+              path: '/home', builder: (context, state) => const HomeScreen()),
+          GoRoute(
+              path: '/home/messages',
+              builder: (context, state) => ConversationsPage()),
+          GoRoute(
+              path: '/home/favorites',
+              builder: (context, state) => const FavoritesScreen()),
+          GoRoute(
+              path: '/home/profile',
+              builder: (context, state) => const ProfileScreen()),
           GoRoute(
             path: '/home/chat/:userName',
             pageBuilder: (context, state) {
@@ -35,15 +45,19 @@ class AppRouter {
                 key: state.pageKey,
                 child: ChatPage(userName: userName, avatar: avatar),
                 transitionsBuilder: (context, animation, _, child) {
-                  final tween = Tween(begin: const Offset(1, 0), end: Offset.zero)
-                      .chain(CurveTween(curve: Curves.easeInOut));
-                  return SlideTransition(position: animation.drive(tween), child: child);
+                  final tween =
+                      Tween(begin: const Offset(1, 0), end: Offset.zero)
+                          .chain(CurveTween(curve: Curves.easeInOut));
+                  return SlideTransition(
+                      position: animation.drive(tween), child: child);
                 },
               );
             },
           ),
           // 👇 Nueva ruta para crear publicación
-          GoRoute(path: '/new_post', builder: (context, state) => const NewPostScreen()),
+          GoRoute(
+              path: '/new_post',
+              builder: (context, state) => const NewPostScreen()),
         ],
       ),
 
@@ -57,7 +71,6 @@ class AppRouter {
         path: '/admin/users',
         builder: (context, state) => AdminUsersPage(),
       ),
-
     ],
   );
 }
