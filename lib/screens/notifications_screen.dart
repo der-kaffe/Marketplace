34d8e53757c1c18a9ec2f98708b18075e93cc1f1
juanCardenasU <juancardenas6217@gmail.com>
2025-10-19 06:26:16 -0300

    pueba

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -101,6 +101,7 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
     }
   }
 
+// comando
   // --- 👇 AÑADE ESTA FUNCIÓN 👇 ---
   String _getTitleForType(String? type) {
     switch (type) {
